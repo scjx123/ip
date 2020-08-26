9cd250a53e814629e82561ee088088d158aaf115
steve <e0407222@u.nus.edu>
2020-08-26 09:20:27 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,8 +10,35 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         //Greets user and exit
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can i do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can i do for you?");
+        Scanner userInput = new Scanner(System.in);
+        String userCommand;
+        while(true){
+            userCommand= userInput.nextLine();
+            System.out.println("    -------------------------");
+            matchCommand(userCommand);
+            System.out.println("    -------------------------");
+            if(userCommand.equals("bye")){
+                System.exit(1);
+            }
+        }
+    }
+
+    public static void matchCommand(String userCommand){
+        switch (userCommand) {
+        case "list": {
+            System.out.println("    list");
+        } break;
+        case "blah": {
+            System.out.println("    blah");
+        } break;
+        case "bye": {
+            System.out.println("    Bye. Hope to see you again soon!");
+        }break;
+        default: {
+        }break;
+        }
     }
 }
+
