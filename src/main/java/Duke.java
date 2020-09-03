fd5044a1a2cf53c0b77f6c6e2148696ff815c5d7
steve <e0407222@u.nus.edu>
2020-09-03 14:40:25 +0800

    Add Coding Standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,85 +11,86 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        //Greets user and exit
+
         System.out.println("    Hello! I'm Duke");
         System.out.println("    What can i do for you?");
-        Scanner sc = new Scanner(System.in);
-        String command;
-        while(true){
-            command = sc.nextLine();
-            System.out.println("-------------------------");
-            user_command(command);
-            System.out.println("-------------------------");
-            if(command.equals("bye")){
+
+        Scanner userInput = new Scanner(System.in);
+
+        String userCommand;
+
+        while(true) {
+            userCommand = userInput.nextLine();
+            System.out.println("    ____________________________________________________________");
+            matchCommand(userCommand);
+            System.out.println("    ____________________________________________________________");
+            if(userCommand.equals("bye")) {
                 System.exit(1);
             }
         }
     }
-    public enum taskType{
-        TODO,
-        DEADLINE,
-        EVENT
-    }
-    public static void user_command(String command) {
-        StringTokenizer st=new StringTokenizer(command);
+    public static void matchCommand(String userCommand) {
+        StringTokenizer st = new StringTokenizer(userCommand);
         String tokenHolder = st.nextToken();
         int dividerPosition = 0;
         int whitespacePosition = 0;
         String dateTime = "";
 
-        if(command.contains("/")){
-            dividerPosition = command.indexOf('/');
-            whitespacePosition = command.indexOf(' ');
-            dateTime=command.substring(dividerPosition+1);
-            command=command.substring(whitespacePosition,dividerPosition);
+        if(userCommand.contains("/")) {
+            dividerPosition = userCommand.indexOf('/');
+            whitespacePosition = userCommand.indexOf(' ');
+            dateTime=userCommand.substring(dividerPosition+1);
+            userCommand=userCommand.substring(whitespacePosition,dividerPosition);
+        } else {
+            if(st.hasMoreTokens()) {
+                userCommand = userCommand.substring(userCommand.indexOf(' '));
+            }
         }
 
         switch (tokenHolder) {
-        case "list": {
+        case "list":
             Task.printList();
-        } break;
-        case "done": {
-            System.out.println("    Nice! I've marked this task as done:");
+            break;
+        case "done":
+            System.out.println("     Nice! I've marked this task as done:");
             int itemNum = Integer.parseInt(st.nextToken())-1;
 
             if(Task.itemExist(itemNum)) {  //First validate if item exist in list
-                Task.completed(itemNum); //if yes, then we call the completed function.
+                Task.markAsDone(itemNum);   //if yes, then we call the completed function.
             }
-        } break;
-        case "bye": {
-            System.out.println("    Bye. Hope to see you again soon!");
-        }break;
-        default: {
+            break;
+        case "bye":
+            System.out.println("     Bye. Hope to see you again soon!");
+            break;
+        default:
             if(tokenHolder.equals("todo")) {
-                new Task(command, taskType.TODO,dateTime);
-            }else if(tokenHolder.equals("deadline")){
-                new Task(command, taskType.DEADLINE,dateTime);
-            }else if(tokenHolder.equals("event")){
-                new Task(command, taskType.EVENT,dateTime);
+                new todo(userCommand);
+            }else if(tokenHolder.equals("deadline")) {
+                new Deadline(userCommand,dateTime);
+            }else if(tokenHolder.equals("event")) {
+                new event(userCommand,dateTime);
             }
             Task.printTask();
-        }break;
+            break;
         }
     }
 }
 
 class Task {
-
     protected String description;
     protected boolean isDone;
-    protected Duke.taskType type;
-    String dateTime="";
+    String dateTime = "";
     static ArrayList<Task> list = new ArrayList<>(); //class level member
 
-    public Task(String description,Duke.taskType type,String dateTime) {
+    public Task(String description) {
         this.description = description;
         this.isDone = false;
-        this.type=type;
-        if(!dateTime.isEmpty()) {
-            this.dateTime = "(" + dateTime + ")";
-        }
         list.add(this);
+    }
+
+    @Override
+    public String toString() {
+        return ("["+getStatusIcon()+"]" +getDescription());
     }
 
     public String getStatusIcon() {
@@ -96,45 +99,67 @@ class Task {
     public String getDescription() {
         return description;
     }
-    public char getType() {
-        char taskTypetoLetter='X';
-        switch(type){
-        case TODO:{
-            taskTypetoLetter='T';
-        }break;
-        case DEADLINE:{
-            taskTypetoLetter='D';
-        }break;
-        case EVENT:{
-            taskTypetoLetter='E';
-        }break;
-        default:
-            break;
-        }
-        return taskTypetoLetter;
-    }
 
-    public static boolean itemExist(int num){
+    public static boolean itemExist(int num) {
         return list.get(num) != null;
     }
-    public static void completed(int num){
+    public static void markAsDone(int num) {
         list.get(num).isDone=true;
         System.out.println("    ["+list.get(num).getStatusIcon()+"]" + list.get(num).getDescription());
     }
-    public static void printList(){
+    public static void printList() {
         int i =1;
-        System.out.println("    Here are the task in your list:");
-        for(Task t : list){
-            System.out.println("    "+i+".["+t.getType()+"]["+t.getStatusIcon()+"]" + t.getDescription()+t.dateTime);
+        System.out.println("     Here are the task in your list:");
+        for(Task t : list) {
+            System.out.println("     "+i+"."+t.toString());
             i++;
         }
     }
-    public static void printTask(){
-        Task t= list.get(list.size()-1); //last item on the list.
-        System.out.println("    Got it. I've added this task:");
-        System.out.println("      ["+t.getType()+"]["+t.getStatusIcon()+"]"
-                + t.getDescription()+t.dateTime);
-        System.out.println("    Now you have "+list.size()+" task in the list");
+    public static void printTask() {
+        Task t = list.get(list.size()-1); //last item on the list.
+        System.out.println("     Got it. I've added this task:");
+        System.out.println("       "+t.toString());
+        System.out.println("     Now you have "+list.size()+" task in the list.");
 
+    }
+}
+
+class todo extends Task {
+    public todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+class Deadline extends Task {
+
+    public Deadline(String description,String dateTime) {
+        super(description);
+        if(!dateTime.isEmpty()) {
+            dateTime=dateTime.replaceFirst(" ",": ");
+            this.dateTime = "(" + dateTime + ")";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + dateTime ;
+    }
+}
+class event extends Task {
+    public event(String description,String dateTime) {
+        super(description);
+        if(!dateTime.isEmpty()) {
+            dateTime=dateTime.replaceFirst(" ",": ");
+            this.dateTime = "("+ dateTime +")" ;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + dateTime ;
     }
 }
